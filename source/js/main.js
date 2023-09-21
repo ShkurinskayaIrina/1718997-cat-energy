43de978df806145eb00a83104702df89c52e348f
ShkurinskayaIrina <shkurinskayaIrina@gmail.com>
2023-09-21 11:05:10 +0300

    Выполнена адаптация сетки внутренних страниц и графики всех страниц

--- source/js/main.js
+++ source/js/main.js
@@ -1,5 +1,4 @@
 const classSliderButttonBefore = 'slider__button--before';
-
 const classSliderBefore = 'slider--before';
 const classSliderAfter = 'slider--after';
 
@@ -7,12 +6,37 @@ const navHeader = document.querySelector('.page-header__nav');
 const navToggle = navHeader.querySelector('.page-header__toggler');
 
 const exampleContainer = document.querySelector('.view');
-const viewSlider = exampleContainer.querySelector('.view__slider');
+if (exampleContainer) {
+  const viewSlider = exampleContainer.querySelector('.view__slider');
 
-const exampleImgBefore = exampleContainer.querySelector('.view__image--before');
-const exampleImgAfter = exampleContainer.querySelector('.view__image--after');
+  const exampleImgBefore = exampleContainer.querySelector('.view__image--before');
+  const exampleImgAfter = exampleContainer.querySelector('.view__image--after');
 
-const sliderButtons = exampleContainer.querySelectorAll('.slider__button');
+  const sliderButtons = exampleContainer.querySelectorAll('.slider__button');
+
+  const handleSliderClick = function(classNameRemove, classNameAdd) {
+    viewSlider.classList.remove(classNameRemove);
+    viewSlider.classList.add(classNameAdd);
+  }
+
+  const showExampleImg = function(elementClassRemove, elementClassAdd) {
+    const className = 'view__image--active';
+    elementClassRemove.classList.remove(className);
+    elementClassAdd.classList.add(className);
+  }
+
+  sliderButtons.forEach((sliderButton) => {
+    sliderButton.addEventListener('click', () => {
+      if (sliderButton.classList[1] === classSliderButttonBefore) {
+        handleSliderClick(classSliderAfter, classSliderBefore);
+        showExampleImg(exampleImgAfter, exampleImgBefore);
+      } else {
+        handleSliderClick(classSliderBefore, classSliderAfter);
+        showExampleImg(exampleImgBefore, exampleImgAfter);
+      }
+    })
+  });
+}
 
 navHeader.classList.remove('page-header__nav--nojs');
 navToggle.addEventListener('click', function() {
@@ -20,25 +44,8 @@ navToggle.addEventListener('click', function() {
   navHeader.classList.toggle('page-header__nav--opened');
 });
 
-const showExampleImg = function(elementClassRemove, elementClassAdd) {
-  const className = 'view__image--active';
-  elementClassRemove.classList.remove(className);
-  elementClassAdd.classList.add(className);
-}
 
-const handleSliderClick = function(classNameRemove, classNameAdd) {
-  viewSlider.classList.remove(classNameRemove);
-  viewSlider.classList.add(classNameAdd);
-}
 
-sliderButtons.forEach((sliderButton) => {
-  sliderButton.addEventListener('click', () => {
-    if (sliderButton.classList[1] === classSliderButttonBefore) {
-      handleSliderClick(classSliderAfter, classSliderBefore);
-      showExampleImg(exampleImgAfter, exampleImgBefore);
-    } else {
-      handleSliderClick(classSliderBefore, classSliderAfter);
-      showExampleImg(exampleImgBefore, exampleImgAfter);
-    }
-  })
-});
+
+
+
